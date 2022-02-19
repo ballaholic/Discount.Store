616a43d43258be328b6c9c2665b60290de9b25b9
Ivan Ivanov <snap4n@gmail.com>
2022-02-20 00:32:26 +0200

    added migration

---
+++ Discount.Store.Infrastructure/Migrations/20220219212212_Alter-CartItem.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Discount.Store.Infrastructure.Migrations
+{
+    public partial class AlterCartItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "CartItems");
+        }
+    }
+}
